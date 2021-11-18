b95c184aa6e8e6e04127fb5bf84f3e8fc759a749
Adam Abernathy <amabernathy@ku.edu>
2021-11-17 21:43:45 -0600

    php updates

--- php/quiz.php
+++ php/quiz.php
@@ -1,13 +1,25 @@
 <?php
 //access the global array called $_POST to get the values from the text fields
-$name = $_POST["name"];
-$email = $_POST["email"];
+// $name = $_POST["name"];
+// $email = $_POST["email"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST("name"));
+  $email = test_input($_POST("email"));
+  $ans_q1 = test_input($_POST("q1"));
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
 echo "Name: " . $name . "<br>";
 echo "Email: " . $email . "<br>";
 
-$ans_1 = $_POST["q1_a2"];
 echo "Question 1: blah blah <br>";
 echo "Correct Answer: this <br>";
-echo "Your Answer: " . $ans_1 . "<br>";
+echo "Your Answer: " . $ans_q1 . "<br>";
 
 ?>
