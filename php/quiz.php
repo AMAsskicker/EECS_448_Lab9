a0241bb996fc2528010b4e8e317a7356d407bf86
Adam Abernathy <amabernathy@ku.edu>
2021-11-17 23:49:34 -0600

    finished quiz backend

--- php/quiz.php
+++ php/quiz.php
@@ -3,15 +3,6 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-//access the global array called $_POST to get the values from the text fields
-// $name = $_POST["name"];
-// $email = $_POST["email"];
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // $name = test_input($_POST("name"));
-  // $email = test_input($_POST("email"));
-  // $ans_q1 = test_input($_POST("q1"));
-// }
-
 $name = $_POST["name"];
 $email = $_POST["email"];
 $ans_q1 = $_POST["q1"];
@@ -24,41 +15,40 @@ $score = 0;
 echo "Name: " . $name . "<br>";
 echo "Email: " . $email . "<br><br>";
 
-echo "Question 1: blah blah <br>";
-echo "Correct Answer: this <br>";
+echo "Question 1: What is the color of the sky? <br>";
+echo "Correct Answer: Blue <br>";
 echo "Your Answer: " . $ans_q1 . "<br><br>";
 if ($ans_q1 == "blue") {
   $score++;
 }
 
-echo "Question 2: blah blah <br>";
-echo "Correct Answer: this <br>";
+echo "Question 2: Does pineapple belong on pizza? <br>";
+echo "Correct Answer: Who cares, eat what you like! <br>";
 echo "Your Answer: " . $ans_q2 . "<br><br>";
-if ($ans_q2 == "like") {
+if ($ans_q2 == "Who cares, eat what you like!") {
   $score++;
 }
 
-echo "Question 3: blah blah <br>";
-echo "Correct Answer: this <br>";
+echo "Question 3: How fast does a fly fly? <br>";
+echo "Correct Answer: 5 MPH <br>";
 echo "Your Answer: " . $ans_q3 . "<br><br>";
-if ($ans_q1 == "blue") {
+if ($ans_q3 == "5 MPH") {
   $score++;
 }
 
-echo "Question 4: blah blah <br>";
-echo "Correct Answer: this <br>";
+echo "Question 4: e^0 = ? <br>";
+echo "Correct Answer: 1 <br>";
 echo "Your Answer: " . $ans_q4 . "<br><br>";
-if ($ans_q1 == "blue") {
+if ($ans_q4 == "1") {
   $score++;
 }
 
-echo "Question 5: blah blah <br>";
-echo "Correct Answer: this <br>";
+echo "Question 5: What was the original name of a Siemen unit? <br>";
+echo "Correct Answer: Mho <br>";
 echo "Your Answer: " . $ans_q5 . "<br><br>";
-if ($ans_q1 == "blue") {
+if ($ans_q5 == "Mho") {
   $score++;
 }
 
-echo "final score: " . $score . "of 5<Br>";
-
+echo "FINAL SCORE: " . $score . " of 5, " . ($score/5)*100 . "%<br><br>";
 ?>
