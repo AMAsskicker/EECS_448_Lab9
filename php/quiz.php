286f53d6727e6cf0b71412e462794effdb855aa2
Adam Abernathy <amabernathy@ku.edu>
2021-11-17 21:17:07 -0600

    quiz question test

--- php/quiz.php
+++ php/quiz.php
@@ -4,4 +4,10 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 echo "Name: " . $name . "<br>";
 echo "Email: " . $email . "<br>";
+
+$ans_1 = $_POST["q1_a2"];
+echo "Question 1: blah blah <br>";
+echo "Correct Answer: this <br>";
+echo "Your Answer: " . $ans_1 . "<br>";
+
 ?>
