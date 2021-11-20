8903e5104fe814d9265439e31be59effd0a30e89
Adam Abernathy <amabernathy@ku.edu>
2021-11-20 13:48:59 -0600

    change html format and php

--- php/customerBackend.php
+++ php/customerBackend.php
@@ -9,23 +9,26 @@ $item_1 = $_POST["item1"];
 $item_2 = $_POST["item2"];
 $item_3 = $_POST["item3"];
 $ship = $_POST["shipping"];
-$subtotal = 0;
 $total;
 
-if ($item_1 == "yes") {
-  $subtotal++;
-}
-if ($item_2 == "yes"){
-  $subtotal++;
-}
-if ($item_3 == "yes"){
-  $subtotal++;
-}
+$subtotal = ($item_1 * 7.99) + ($item_2 * 7.89) + ($item_3 * 6.99);
+// if ($item_1 == "yes") {
+//   $subtotal += 7.99;
+// }
+// if ($item_2 == "yes"){
+//   $subtotal++;
+// }
+// if ($item_3 == "yes"){
+//   $subtotal++;
+// }
+
 
 if ($ship == "overnight") {
-  $total = $subtotal * 7.99 + 50;
+  $total = $subtotal + 50;
 } elseif ($ship == "three") {
   $total = $subtotal * 7.99 + 5;
+} else {
+  $total = $subtotal * 7.99;
 }
 
 echo "<html>";
@@ -38,19 +41,45 @@ echo "<body>";
 echo '<span id="user_pass">User Name: ' . $user . '</span><br>';
 echo '<span id="user_pass">Password: ' . $u_pass . '</span><br><br>';
 
-echo '<span id="price">Purchase Item 1: '.$item_1.'</sapn><br>';
-echo '<span id="price">Purchase Item 2: '.$item_3.'</sapn><br>';
-echo '<span id="price">Purchase Item 3: '.$item_3.'</sapn><br>';
+echo 'Red Spatula x '.$item_1.' =  '.$item_1 * 7.89'<br>';
+
+// echo '<span id="price">Purchase Red Spatula: '.$item_1.'</sapn><br>';
+// echo '<span id="price">Purchase Blue Spatula: '.$item_3.'</sapn><br>';
+// echo '<span id="price">Purchase Purple Spatula: '.$item_3.'</sapn><br>';
 
 if ($ship == "free") {
-  echo "Free 7 Day<br>";
+  echo '<span id="">Shipping: Overnight = $50</span><br>';
 } elseif ("three") {
-  echo "Shipping: Three Day $5<br>";
+  echo '<span id="">Shipping: Three Day = $5</span><br>';
 } else {
-  echo "Shipping: Overnight $50<br>";
+  echo '<span id="">Shipping: Free 7 Day = $0</span><br>';
 }
 
+echo '<span id="total">TOTAL: $'.$total.'</span><br>';
+
 echo '<span>Total: $'.$total.'</span>';
+echo '<span id="footer">THANK YOU FOR YOUR PURCHASE!!</span><br>';
 echo "</body>";
 echo "</html>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  ?>
