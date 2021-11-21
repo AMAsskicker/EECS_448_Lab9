b892a87dfb421f4596a895c17e95b208259110b1
Adam Abernathy <amabernathy@ku.edu>
2021-11-21 15:38:29 -0600

    added items to store

--- php/customerBackend.php
+++ php/customerBackend.php
@@ -8,12 +8,16 @@ $u_pass = $_POST["password"];
 $item_1 = $_POST["item1"];
 $item_2 = $_POST["item2"];
 $item_3 = $_POST["item3"];
+$item_4 = $_POST["item4"];
+$item_5 = $_POST["item5"];
 $ship = $_POST["shipping"];
 $total;
 $item1_total = $item_1 * 7.99;
 $item2_total = $item_2 * 7.89;
 $item3_total = $item_3 * 6.99;
-$subtotal = $item1_total + $item2_total + $item3_total;
+$item4_total = $item_4 * 8.99;
+$item5_total = $item_5 * 5.99;
+$subtotal = $item1_total + $item2_total + $item3_total + $item4_total + $item5_total;
 if ($ship == "overnight") {
   $total = $subtotal + 50;
 } elseif ($ship == "three") {
@@ -36,7 +40,9 @@ echo '<span id="recipt">    Recipt     </span><br>';
 echo '<span id="recipt">----------</span><br>';
 echo '<span id="recipt">Red Spatula x '.$item_1.' =  '.$item1_total.'</span><br>';
 echo '<span id="recipt">Blue Spatula x '.$item_2.' =  '.$item2_total.'</span><br>';
-echo '<span id="recipt">Red Spatula x '.$item_3.' =  '.$item3_total.'</span><br>';
+echo '<span id="recipt">Purple Spatula x '.$item_3.' =  '.$item3_total.'</span><br>';
+echo '<span id="recipt">White Spatula x '.$item_4.' =  '.$item4_total.'</span><br>';
+echo '<span id="recipt">Black Spatula x '.$item_5.' =  '.$item5_total.'</span><br>';
 echo '<span id="recipt">----------</span><br>';
 echo '<span id="recipt">SUBTOTOAL: $'.round($subtotal,2).'</span><br>';
 echo '<span id="recipt">----------</span><br>';
